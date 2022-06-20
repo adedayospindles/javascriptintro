479679007e1b59a9235e1c6c1f0dce7df650a07a
Adedayo Agboola <kreativedayo@gmail.com>
2022-06-20 20:34:31 +0100

    Introduction to Javascript

---
+++ intro.js
@@ -0,0 +1,9 @@
+// Write a javascript code that prints out your name, height, and country on the screen
+
+const myname ="Adedayo Agboola";
+let height = "1.66";
+let country = "Nigeria";
+let output = "My name is " + myname + ", height is " + height + "m and I am from " + country + ".";
+
+console.log(output);
+// document.write(output);
